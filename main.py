f11b5c60dcac09ab3b5c7c18d3cfcf843a12f2ef
Connor McCary <143134100+connorm16755@users.noreply.github.com>
2024-06-25 09:14:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from agents.chat_service import ChatService
-from agents.memory_retrieval_agent import MemoryRetrievalAgent
-from agents.memory_creation_agent import MemoryCreationAgent
-from tests.performance_evaluation import performance_evaluation
 
 
 chat_svc = ChatService("Connor")
@@ -11,6 +8,3 @@ while True:
         break
     response = chat_svc.invoke(user_input)
     print("AI:",response)
-
-
-
